4b328cf5a7d7d802d75219b5bbdc46d278a95b6d
ashutoshdev <ashutosh2432@gmail.com>
2014-09-27 11:22:05 +0530

    Revert "Revert "Create upload_max_file_size.php""
    This reverts commit efb7b271e33d877bcaed73bae386690ccd85aa15.

---
+++ upload_max_file_size.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  Plugin Name: ashutosh_upload_max_file_size
+  Description: increase, upload, limit, up to, 250Mb
+  Author:Ashutosh Kumar
+  Plugin URI: https://www.facebook.com/ashutosh.kr.upadhyay
+ */
+add_action('admin_menu', 'mt_add_pages');
+function mt_add_pages() {
+   add_menu_page(__('upload_max_file_size','menu-test'), __('max_file_size','menu-test'), 'manage_options', 'upload_max_file_size', 'upload_max_file_size' );
+}
+function upload_max_file_size() {
+    echo "<h2>" . __('Increase Upload Maximum File Size', 'menu-test') . "</h2>";
+    ?>
+    <form method="post">
+        <input type="text" id="color" name="color" value="<?php echo get_option('max_file_size'); ?>"/>
+        
+            
+        <input type="submit" name="submit" value="submit">
+        <br/>(Example: Like 262144000 bytes = 250MB )
+    </form>
+    <?php
+    if (isset($_POST['submit'])) {
+        $color = $_POST['color'];
+        update_option('max_file_size', $color);
+    }
+}
+add_filter('upload_size_limit', 'ashu_increase_upload');
+function ashu_increase_upload($bytes) {
+return get_option('max_file_size'); 
+}
+?>
